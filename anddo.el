6ca2b55c244f27b34facedc4c909f01b5de20645
Lars Ingebrigtsen <larsi@gnus.org>
2024-06-17 01:20:09 +0200

    Add an import function

--- anddo.el
+++ anddo.el
@@ -20,7 +20,7 @@
 
 (defconst anddo--tables
   '((item
-     (id integer)
+     (id integer :primary)
      (status text)
      (subject text)
      (body text)
@@ -28,10 +28,39 @@
      (modification-time text))))
 
 (defun anddo--create-tables ()
+  (sqorm-open (expand-file-name "anddo.sqlite3" user-emacs-directory))
   (sqorm-create-tables anddo--tables))
-  
-    
-     
+
+(defun anddo-import-text-file (file)
+  "Import a text file into anddo."
+  (interactive "fFile to import: ")
+  (with-temp-buffer
+    (insert-file-contents file)
+    (let ((status nil)
+	  (subject "")
+	  (body ""))
+      (while (not (eobp))
+	(cond
+	 ((looking-at "\\([^: ]+\\):")
+	  (setq status (match-string 1)))
+	 ((or (looking-at "[*] \\(.*\\)")
+	      (looking-at "\n"))
+	  (let ((new-subject (match-string 1)))
+	    (when (and status subject)
+	      (sqorm-insert
+	       (sqorm-make
+		'item
+		(list nil (downcase status)
+		      subject
+		      body
+		      (format-time-string "%F %T")
+		      nil)
+		anddo--tables))
+	      (setq body ""))
+	    (setq subject new-subject)))
+	 ((looking-at "  \\(.*\\)")
+	  (setq body (concat body (match-string 1)))))
+	(forward-line 1)))))
 
 (provide 'anddo)
 
