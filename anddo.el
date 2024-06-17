26289a7c1590b9bef76ad46cb7fcbc5cf5952192
Lars Ingebrigtsen <larsi@gnus.org>
2024-06-17 03:29:18 +0200

    Continue implementation

--- anddo.el
+++ anddo.el
@@ -18,6 +18,10 @@
 (require 'sqorm)
 (require 'cl-lib)
 
+(defvar anddo-listing-mode 'new
+  "Default listing mode for anddo.
+Possible values are `new', `all' and `most'.")
+
 (defconst anddo--tables
   '((item
      (id integer :primary)
@@ -32,7 +36,13 @@
   (sqorm-create-tables anddo--tables))
 
 (defun anddo-import-text-file (file)
-  "Import a text file into anddo."
+  "Import a text file into anddo.
+The text file is on the format:
+
+New:
+* Foo bar
+  Something here
+* Zot"
   (interactive "fFile to import: ")
   (with-temp-buffer
     (insert-file-contents file)
@@ -61,6 +71,74 @@
 	 ((looking-at "  \\(.*\\)")
 	  (setq body (concat body (match-string 1)))))
 	(forward-line 1)))))
+
+(defun anddo ()
+  "Display the todo list."
+  (interactive)
+  (pop-to-buffer "*anddo*")
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (anddo-mode)
+    (make-vtable
+     :columns '("More" "Status" "Item")
+     :objects (sqorm-select-where
+	       (format
+		"select * from item %s order by id"
+		(cond
+		 ((eq anddo-listing-mode 'new)
+		  "where status = 'new'")
+		 ((eq anddo-listing-mode 'all)
+		  "")
+		 (t
+		  "where status in ('new', 'possibly', 'in-progress')"))))
+     :getter
+     (lambda (item column vtable)
+       (pcase (vtable-column vtable column)
+	 ("More" (if (length= (plist-get item :body) 0)
+		     ""
+		   "⬇️"))
+	 ("Status" (if (equal (plist-get item :status) "new")
+		       ""
+		     (plist-get item :status)))
+	 ("Item" (plist-get item :subject))))
+     :keymap anddo-mode-map)))
+
+(defvar anddo-statuses '("new" "done" "in-progress"
+			 "possibly" "not-doing"))
+
+(defvar-keymap anddo-mode-map
+  "n" #'anddo-new-item
+  "l" #'anddo-toggle-listing-mode
+  "<RET>" #'anddo-show-body
+  "<DEL>" #'anddo-delete-item)
+
+(define-derived-mode anddo-mode special-mode "anddo"
+  "Major mode for listing todo lists."
+  (setq truncate-lines t))
+
+(defun anddo-toggle-listing-mode ()
+  "Cycle through three listing modes: New-only, non-closed, all."
+  (interactive)
+  (let ((id (plist-get (vtable-current-object) :id)))
+    (setq anddo-listing-mode
+	  (cl-case anddo-listing-mode
+	    ('new 'more)
+	    ('more 'all)
+	    ('all 'new)))
+    (anddo)
+    (when id
+      (when-let ((match (text-property-search-forward
+			 'vtable-object id (lambda (v item)
+					     (= v (plist-get item :id))))))
+	(goto-char (prop-match-beginning match))))))
+
+(defun anddo-show-body ()
+  "Display the body of an item, if any."
+  (interactive)
+  (let ((body (plist-get (vtable-current-object) :body)))
+    (if (zerop (length body))
+	(user-error "No body for the current item")
+      (message "%s" body))))
 
 (provide 'anddo)
 
